ca98b9cc4824ca571727e3b15962b842d52cdbf4
Nick Ye <nick.ye888@gmail.com>
2017-11-28 20:12:21 +1300

    game is fully playable

--- TicTakToeKata/TicTakToeKata/Program.cs
+++ TicTakToeKata/TicTakToeKata/Program.cs
@@ -7,65 +7,83 @@ namespace TicTakToeKata
     {
         static void Main()
         {
-            var game = new Game();
-            var checker = new Checker(3, 3);
-            var moves = new Move();
-            var moveValidator = new MoveValidator();
+            var isNewGame = true;
 
-            var board = game.NewBoard();
-
-            while (!game.IsFinished(board))
+            while (isNewGame)
             {
-                if (checker.CheckWinCondition(board) || game.IsFinished(board))
+                var game = new Game();
+                var checker = new Checker(3, 3);
+                var moves = new Move();
+                var moveValidator = new MoveValidator();
+
+                var board = game.NewBoard();
+
+                while (!game.IsFinished(board))
                 {
-                    Console.WriteLine(Renderer.Render(board));
-                    break;
-                }
-                var computerMove = moves.ComputerPlayersMove(board);
+                    if (checker.CheckWinCondition(board) || game.IsFinished(board))
+                    {
+                        Console.WriteLine(Renderer.Render(board));
+                        break;
+                    }
+                    var computerMove = moves.ComputerPlayersMove(board);
 
-                Console.WriteLine("Computer (x) moves at " + computerMove);
+                    Console.WriteLine("Computer (x) moves at " + computerMove);
 
-                if (checker.CheckWinCondition(board) || game.IsFinished(board))
-                {
-                    Console.WriteLine(Renderer.Render(board));
-                    break;
-                }
+                    if (checker.CheckWinCondition(board) || game.IsFinished(board))
+                    {
+                        Console.WriteLine(Renderer.Render(board));
+                        break;
+                    }
 
-                Console.WriteLine(Renderer.Render(board));
-
-                Console.WriteLine("To make a move, please enter a number from 0-8");
-
-                var userInput = Console.ReadLine();
-
-                var validInput = game.IsValidInput(userInput);
-
-                ValidInputPrompt(game, ref userInput, ref validInput, board);
-
-                var userMove = Convert.ToInt32(userInput);
-
-                while (!moveValidator.IsValidMove(board, userMove))
-                {
-                    Console.WriteLine("Sorry invalid move, please choose an empty space");
                     Console.WriteLine(Renderer.Render(board));
 
-                    userInput = Console.ReadLine();
-                    validInput = game.IsValidInput(userInput);
+                    Console.WriteLine("To make a move, please enter a number from 0-8");
+
+                    var userInput = Console.ReadLine();
+
+                    var validInput = game.IsValidInput(userInput);
+
                     ValidInputPrompt(game, ref userInput, ref validInput, board);
 
+                    var userMove = Convert.ToInt32(userInput);
+
+                    while (!moveValidator.IsValidMove(board, userMove))
+                    {
+                        Console.WriteLine("Sorry invalid move, please choose an empty space");
+                        Console.WriteLine(Renderer.Render(board));
+
+                        userInput = Console.ReadLine();
+                        validInput = game.IsValidInput(userInput);
+                        ValidInputPrompt(game, ref userInput, ref validInput, board);
+
+                        userMove = Convert.ToInt32(userInput);
+                    }
+
                     userMove = Convert.ToInt32(userInput);
+
+                    moves.UsersMove(board, userMove);
+
+                    Console.WriteLine("player (o) at " + userInput);
+
+
                 }
 
-                userMove = Convert.ToInt32(userInput);
+                if (game.IsFinished(board))
+                {
+                    Console.WriteLine("Draw!");
+                }
 
-                moves.UsersMove(board, userMove);
+                if (checker.CheckWinCondition(board))
+                {
+                    Console.WriteLine("Winner!");
+                }
 
-                Console.WriteLine("player (o) at " + userInput);
+                Console.WriteLine("Play again? y/n");
+                var gamePlayIntent = Console.ReadLine();
 
+                isNewGame = gamePlayIntent == "y";
                 
             }
-
-            Console.WriteLine("Winner!");
-            Console.ReadLine();
 
         }
 
